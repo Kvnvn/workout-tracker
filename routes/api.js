e5066f63d5d9f7223aaaafed3c367f7d260a7ca0
Kevin Nguyen <60084108+Kvnvn@users.noreply.github.com>
2020-04-29 21:43:45 -0700

    added in new schema

--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,19 @@
 const router = require("express").Router();
 
 // TODO: import required model/s
-
+const db = require("../models");
 // TODO: and add code to the routes so that the app functions correctly
 
 // Creates a workout using data in the request body.
 router.post("/api/workouts", (req, res) => {
-  // CODE HERE
+  db.Workout.create(req.body)
+  .then((data) => {
+    console.log(data);
+    res.json(data);
+  })
+  .catch((err) => {
+    res.json(err);
+  });
 });
 
 // Respond with workout for id url parameter. This should
